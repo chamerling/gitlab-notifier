462ae0ff02c28109a9ed66de8c64f28fac3e589c
Christophe Hamerling <chamerling@linagora.com>
2018-10-29 12:23:05 +0100

    Add electron and display as menu app

---
+++ app.js
@@ -0,0 +1,15 @@
+const menubar = require('menubar');
+
+let url;
+if (process.env.NODE_ENV === 'DEV') {
+  require('electron-debug')({ showDevTools: true })
+  url = 'http://localhost:8080/';
+} else {
+  url = `file://${process.cwd()}/dist/index.html`
+}
+
+const mb = menubar({ index: url, icon: `${process.cwd()}/src/assets/icon.png` });
+
+mb.on('ready', () => {
+  console.log('menubar app is ready')
+});
